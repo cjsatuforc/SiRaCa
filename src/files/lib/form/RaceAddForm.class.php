8f4441c38dcf7fe5019dfaed0d49fde9dbada085
Damien Laurent <hiboudev@gmail.com>
2018-10-22 11:30:52 +0200

    display race after creating it

--- src/files/lib/form/RaceAddForm.class.php
+++ src/files/lib/form/RaceAddForm.class.php
@@ -4,7 +4,9 @@ namespace wcf\form;
 use wcf\data\siraca\race\RaceAction;
 use wcf\form\AbstractForm;
 use wcf\system\exception\UserInputException;
+use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
+use wcf\util\HeaderUtil;
 use wcf\util\StringUtil;
 
 class RaceAddForm extends AbstractForm
@@ -45,14 +47,13 @@ class RaceAddForm extends AbstractForm
             ]),
         ]);
 
-        $this->objectAction->executeAction();
+        $race = $this->objectAction->executeAction()['returnValues'];
 
         $this->saved();
 
-        $this->title = '';
-
-        // show success message
-        WCF::getTPL()->assign('success', true);
+        HeaderUtil::redirect(LinkHandler::getInstance()->getLink('Race', [
+            'object' => $race,
+        ]));
     }
 
     public function assignVariables()
